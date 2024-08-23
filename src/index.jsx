23bd17edafed1fbab033fd32f4da036b34fca9b1
ThwDog <thanadul.sod.work@gmail.com>
2024-08-24 00:02:47 +0700

    add deploy

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import Header from './Header';
 import reportWebVitals from './reportWebVitals';
 import Arrow from './Arrow';
-
-import Home from './section/Home.jsx';
-import AboutMe from './section/AboutMe.jsx';
-import MyWork from './section/MyWork.jsx';
-import Contract from './section/Contract.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
